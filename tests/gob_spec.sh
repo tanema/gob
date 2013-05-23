879d1a4aca94a5c5bdb8f96d12d563d048f159f5
Tim Anema <tim@zenapsis.com>
2013-05-23 10:58:22 -0400

    finished up the tests

--- tests/gob_spec.sh
+++ tests/gob_spec.sh
@@ -2,34 +2,35 @@
 
 green='\033[32m'
 red='\033[31m'
+yellow='\033[33m'
 end_c='\033[0m'
 
 mkdir "tmp"
 cd tmp
-echo "gob help should show message"
+printf "gob help should show message ..."
 if [[ "x$(gob help)" = "x" ]]; then
   echo "${red}Failed with $gob_help${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-echo "a unrecognized command should echo the help info"
+printf "a unrecognized command should echo the help info ..."
 if [[ "$(gob help)" != "$(gob fuck)" ]]; then
   echo "${red}Failed${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-echo "gob init should create a .goproj file in the pwd, marking it as the root of a project."
-mkdir test && cd test && gob init
+printf "gob init should create a .goproj file in the pwd, marking it as the root of a project. ..."
+mkdir test && cd test && echo "y" | gob init &> /dev/null
 if [ -f ".goproj" ]; then
   echo "${green}Passed${end_c}"
 else
   echo "${red}Failed${end_c}"
 fi
 
-echo "gob init should not allow to create a project inside a project"
-mkdir test2 && cd test2 && gob init
+printf "gob init should not allow to create a project inside a project ..."
+mkdir test2 && cd test2 && gob init &> /dev/null 
 if [ -f ".goproj" ]; then
   echo "${red}Failed${end_c}"
 else
@@ -37,16 +38,16 @@ else
 fi
 cd ../.. && rm -rf test
 
-echo "gob new should create a new directory with a .goproj and .gitignore file in it."
-gob new test && cd test
+printf "gob new should create a new directory with a .goproj and .gitignore file in it. ..."
+gob new test &> /dev/null && cd test
 if [ -f ".goproj" ] && [ -f ".gitignore" ]; then
   echo "${green}Passed${end_c}"
 else
   echo "${red}Failed${end_c}"
 fi
 
-echo "gob new should not allow to create a project inside a project"
-mkdir test2 && cd test2 && gob new test
+printf "gob new should not allow to create a project inside a project ..."
+mkdir test2 && cd test2 && gob new test &> /dev/null
 if [ -d "test" ]; then
   echo "${red}Failed${end_c}"
 else
@@ -54,20 +55,28 @@ else
 fi
 cd ../.. && rm -rf test
 
-echo "gob version should display a version Number."
+printf "gob version should display a version Number...."
 if [ $(gob version) != $(echo $GOB_VERSION) ]; then
   echo "${red}Failed${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-echo "changing directories in to a project should set the path gopath and goproj_path"
-gob new test && cd test
-if [ "$GOPATH" != "$PWD" ] || [ "$GOPROJ_PATH" != "$PWD" ] || [ $PATH != $GOPATH* ]; then
+printf "changing directories into a project should set the path gopath and goproj_path ..."
+gob new test &> /dev/null  && cd test
+if [ "$GOPATH" != "$PWD" ] || [[ $PWD != $GOPROJ_PATH* ]] || [[ $PATH != $PWD* ]]; then
   echo "${red}Failed${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-cd ../..
+printf "changing out of a directory should clear the variables ..."
+cd ..
+if [ "$GOPATH" = "" ] && [ "$GOPROJ_PATH" = "" ] && [ $PATH != $GOPATH* ]; then
+  echo "${green}Passed${end_c}"
+else
+  echo "${red}Failed${end_c}"
+fi
+
+cd ..
 rm -rf tmp
--- tests/reveler_spec.sh
+++ tests/reveler_spec.sh
@@ -2,34 +2,35 @@
 
 green='\033[32m'
 red='\033[31m'
+yellow='\033[33m'
 end_c='\033[0m'
 
 mkdir "tmp"
 cd tmp
-echo "reveler help should show message"
+printf "reveler help should show message ..."
 if [[ "x$(reveler help)" = "x" ]]; then
   echo "${red}Failed with $reveler_help${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-echo "a unrecognized command should echo the help info"
+printf "a unrecognized command should echo the help info ..."
 if [[ "$(reveler help)" != "$(reveler fuck)" ]]; then
   echo "${red}Failed${end_c}"
 else
   echo "${green}Passed${end_c}"
 fi
 
-echo "reveler init should create a .goproj file in the pwd, marking it as the root of a project."
-mkdir test && cd test && reveler init
+printf "reveler init should create a .goproj file in the pwd, marking it as the root of a project. ..."
+mkdir test && cd test && echo "y" | reveler init &> /dev/null
 if [ -f ".goproj" ]; then
   echo "${green}Passed${end_c}"
 else
   echo "${red}Failed${end_c}"
 fi
 
-echo "reveler init should not allow to create a project inside a project"
-mkdir test2 && cd test2 && reveler init
+printf "reveler init should not allow to create a project inside a project ..."
+mkdir test2 && cd test2 && reveler init &> /dev/null
 if [ -f ".goproj" ]; then
   echo "${red}Failed${end_c}"
 else
@@ -37,26 +38,30 @@ else
 fi
 cd ../.. && rm -rf test
 
-# echo "reveler new should create a new directory with a .goproj and .gitignore file in it."
-# reveler new test && cd test
-# if [ -f ".goproj" ] && [ -f ".gitignore" ]; then
-#   echo "${green}Passed${end_c}"
-# else
-#   echo "${red}Failed${end_c}"
-# fi
-# 
-# echo "reveler new should not allow to create a project inside a project"
-# mkdir test2 && cd test2 && reveler new test
-# if [ -d "test" ]; then
-#   echo "${red}Failed${end_c}"
-# else
-#   echo "${green}Passed${end_c}"
-# fi
-# cd ../.. && rm -rf test
-# reveler ini
-# reveler debug
+printf "reveler new should create a new directory with a .goproj and .gitignore file in it as well as revel. ..."
+reveler new test &> /dev/null
+if [ -f "../../.goproj" ] && [ -f "../../.gitignore" ] && [ -f "../../bin/revel" ] && [ -d "app" ]; then
+  echo "${green}Passed${end_c}"
+else
+  echo "${red}Failed${end_c}"
+fi
+cd ../..
 
-echo "reveler version should display a version Number."
+printf "reveler new should not create a new project inside another project ..."
+mkdir test2 && cd test2 && reveler new test &> /dev/null
+if [ -d "test" ]; then
+  echo "${red}Failed${end_c}"
+else
+  echo "${green}Passed${end_c}"
+fi
+cd ../.. && rm -rf test
+
+printf "reveler run should call revel run on the project ..."
+echo "${yellow}Pending${end_c}"
+printf "reveler debug should start up a session of gdb with the app loaded ..."
+echo "${yellow}Pending${end_c}"
+
+printf "reveler version should display a version Number. ..."
 if [ $(reveler version) != $(echo $GOB_VERSION) ]; then
   echo "${red}Failed${end_c}"
 else
